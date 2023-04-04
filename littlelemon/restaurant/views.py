b2262bbe808de08613671281572d0ff54c7c5577
AlpTuna <alptuna2003@hotmail.com>
2023-04-04 22:15:54 +0200

    Added the static files

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def sayHello(request):
+ return HttpResponse('Hello World')
+
+def index(request):
+    return render(request, 'index1.html', {})
